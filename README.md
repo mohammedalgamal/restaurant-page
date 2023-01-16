2433d861ab83f59d2aeb06eba9afbace70418919
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-01-16 18:23:46 +0200

    Add all required dependencies

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # restaurant-page
+
 Practicing DOM manipulation by dynamically rendering a simple restaurant homepage using JavaScript alone to generate the entire contents of the website.
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
-const path = require('path');
+const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    home: "./src/home.js",
+    menu: "./src/menu.js",
+    contact: "./src/contact.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
