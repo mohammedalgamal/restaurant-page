5a60c308e686f7895fe2c31b3df9f3aa82732cb2
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-01-21 18:33:43 +0200

    Add footer and its content and style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
+import gitHubImg from "./GitHub-Mark-Light-32px.png";
+
+// Add header and its content
+const header = document.createElement("div");
+header.className = "header";
+document.body.appendChild(header);
+
+// Add main section 'content' and its content
+const content = document.createElement("div");
+content.className = "content";
+document.body.appendChild(content);
+
+// Add footer and its content
+const footer = document.createElement("div");
+footer.className = "footer";
+
+const footerText = document.createElement("div");
+footerText.className = "text";
+footerText.innerHTML = "Copyright &#169 mohammedalgamal";
+
+const footerImgLink = document.createElement("a");
+footerImgLink.href = "https://github.com/mohammedalgamal/restaurant-page";
+
+const footerImg = document.createElement("img");
+footerImg.src = gitHubImg;
+footerImg.alt = "GitHub Logo";
+
+footerImgLink.appendChild(footerImg);
+footerText.appendChild(footerImgLink);
+footer.appendChild(footerText);
+document.body.appendChild(footer);
